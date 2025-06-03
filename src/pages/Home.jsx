c3ab113916f10dbd513b7e39da3a3819ed71dbc1
Ayush Chaurasiya <ayushchaurasiya.080@gmail.com>
2025-06-03 22:35:08 +0530

    Your commit message here

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,14 +18,6 @@ const Home = () => {
         <source src={video} type="video/mp4" />
         Your browser does not support the video tag.
     </video>
-
-    <div className="absolute top-0 left-0 w-full h-full bg-black/50 flex flex-col items-center justify-center text-center text-white px-4">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Our Website</h1>
-        <p className="text-lg md:text-xl mb-6">We bring you the best of services and innovation.</p>
-        <button onClick={()=>Navigate('/work')} className="bg-primary hover:bg-black text-black hover:text-primary font-semibold py-2 px-6 rounded">
-        Get Started
-        </button>
-    </div>
     </div>
 
   );
